568c6a0360bdbf347d1afd2d3106b88fa97a9354
David Miller <David>
2012-02-02 21:01:54 +0000

    Hack

--- life.py
+++ life.py
@@ -1,14 +1,64 @@
-WIDTH = 10
-HEIGHT = 10
+import os
+import random
+import time
+import colorama
+import sys
+
+from colorama import init, Back
+init()
+
+WIDTH = 40
+HEIGHT = 180 # Not really
+
+
+
+def printer(board):
+    """
+    Print a board
+
+    Arguments:
+    - `board`:
+    """
+    os.system("clear")
+    print "-" * HEIGHT
+    for row in board:
+        sys.stdout.write("|")
+        for cell in row:
+            if cell:
+                sys.stdout.write(Back.RED + "*" + Back.RESET)
+            else:
+                sys.stdout.write(" ")
+        sys.stdout.write("|\n")
+    print "-" * HEIGHT
+
+def create_board():
+    """
+    Create a random board
+    """
+    board = [[None for x in xrange(HEIGHT)] for x in xrange(WIDTH)]
+    for row in board:
+        for i, cell  in enumerate(row):
+            if i % random.randint(1, 10) == 0:
+                row[i] = True
+    return board
+
+
+def life():
+    """
+    The game of life
+    """
+    printer(create_board())
+
 
 def main():
     """
     GO
     """
-    board = [[[] for x in xrange(HEIGHT)] for x in xrange(WIDTH)]
-    for row in board:
-        print row
-
+    while True:
+        life()
+        time.sleep(0.5)
+        print "Thinking....."
+        time.sleep(random.randint(1, 3))
 
 if __name__ == '__main__':
     main()
